2c216ce4a8f2d84fa2f3d687909516e29b8e0879
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2023-04-15 22:11:02 +1000

    show the pets in the home page and pet_details when you click on the pet name.

--- wisdompests/adoptions/models.py
+++ wisdompests/adoptions/models.py
@@ -13,5 +13,9 @@ class Pet(models.Model):
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
-    class Vaccine(models.Model):
-        name = models.CharField(max_length=50)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
